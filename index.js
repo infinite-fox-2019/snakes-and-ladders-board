b974e2a6ce569a2355f089bf324825fd55a825b0
adityain98 <adityain98@gmail.com>
2019-09-04 15:41:08 +0700

    perbaikan

--- index.js
+++ index.js
@@ -1,58 +1,67 @@
+function numberGeneratorGenap(arr, besarMap) {
+    let blok = besarMap * besarMap;
+    for(let i = 0; i<arr.length; i++){
+        if(i == 0){
+            for(let j = blok; j>0; j--){
+                if(j <= blok - besarMap){
+                break;
+                }
+                arr[i].push(j);
+            }
+        }
+        else{
+            if(i % 2 == 1){
+                for(let j = blok - (besarMap * (i + 1)) + 1; j<=blok - (besarMap * i); j++){
+                    arr[i].push(j)
+                }
+            }
+            else{
+                for(let j = blok - (besarMap * i); j> blok - (besarMap * (i + 1)); j--){
+                    arr[i].push(j)
+                }
+            }
+        }
+    }
+    return arr;
+}
+function numberGeneratorGanjil(arr, besarMap) {
+    let blok = besarMap * besarMap;
+    for(let i = 0; i<arr.length; i++){
+        if(i == 0){
+            for(let j = blok - (besarMap * 1) + 1; j<=blok; j++){
+                arr[i].push(j);
+            }
+        }
+        else{
+            if(i % 2 == 1){
+                for(let j = blok - (besarMap * i); j> blok - (besarMap * (i + 1)); j--){
+                    arr[i].push(j)
+                }
+            }
+            else{
+                for(let j = blok - (besarMap * (i + 1)) + 1; j<=blok - (besarMap * i); j++){
+                    arr[i].push(j)
+                }
+            }
+        }
+    }
+    return arr;
+}
+
 function ularTangga(besarMap) {
     let mapKosong = [];
-
+    let map = [];
     for(let i = 0; i<besarMap; i++){
         mapKosong.push([]);
     }
 
     if(besarMap % 2 == 0){
-        let blok = besarMap * besarMap;
-        for(let i = 0; i<mapKosong.length; i++){
-            if(i == 0){
-                for(let j = blok; j>0; j--){
-                    if(j <= blok - besarMap){
-                    break;
-                    }
-                    mapKosong[i].push(j);
-                }
-            }
-            else{
-                if(i % 2 == 1){
-                    for(let j = blok - (besarMap * (i + 1)) + 1; j<=blok - (besarMap * i); j++){
-                        mapKosong[i].push(j)
-                    }
-                }
-                else{
-                    for(let j = blok - (besarMap * i); j> blok - (besarMap * (i + 1)); j--){
-                        mapKosong[i].push(j)
-                    }
-                }
-            }
-        }
+        map = numberGeneratorGenap(mapKosong, besarMap);
     }
     else{
-        let blok = besarMap * besarMap;
-        for(let i = 0; i<mapKosong.length; i++){
-            if(i == 0){
-                for(let j = blok - (besarMap * 1) + 1; j<=blok; j++){
-                    mapKosong[i].push(j);
-                }
-            }
-            else{
-                if(i % 2 == 1){
-                    for(let j = blok - (besarMap * i); j> blok - (besarMap * (i + 1)); j--){
-                        mapKosong[i].push(j)
-                    }
-                }
-                else{
-                    for(let j = blok - (besarMap * (i + 1)) + 1; j<=blok - (besarMap * i); j++){
-                        mapKosong[i].push(j)
-                    }
-                }
-            }
-        }
+        map = numberGeneratorGanjil(mapKosong, besarMap);
     }
-    return mapKosong;
+    return map;
 }
 
 console.log(ularTangga(10));
