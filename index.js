03e48790c76df40feeddcbf33e9f0459feec52fb
ericsudhartio <*ericsudhartio2509@gmail.com>
2019-09-04 18:31:58 +0700

    fix bug

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ function generateBoard (input){
     let calculate = input * input; 
     for(let i=input; i>0; i--){
         let arr = [];
+        result.push(arr);
         if(i%2!==0){
             for(let j=input-1; j>=0; j--){
-                arr.push(calculate);
+                arr[j] = calculate;
                 calculate--
             }
         }else{
             for(let j=0; j<input; j++){
-                arr.push(calculate);
+                arr[j] = calculate;
                 calculate--
             }
         }
-        result.push(arr);
     }
     return result
 }
